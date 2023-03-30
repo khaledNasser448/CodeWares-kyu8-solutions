fbb1a9f7c929b156b3f03bc0d1a2a55d27d938f3
khaled <dr.khalednasser2020@gmail.com>
2023-03-30 04:45:18 +0200

    counting sheep

---
+++ counting sheep.js
@@ -0,0 +1,13 @@
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+function countSheeps(arrayOfSheep) {
+    counter = 0;
+    let sheep = arrayOfSheep;
+    sheep.map((ele) =>  ele === true? counter++ : counter  );
+    return counter;
+}
+console.log(countSheeps([true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true]));
