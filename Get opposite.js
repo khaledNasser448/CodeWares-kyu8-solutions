ed085408365e0cd5b04511fb4ad777e8ce2c268e
khaled <dr.khalednasser2020@gmail.com>
2023-03-30 17:17:24 +0200

    find opposte

---
+++ Get opposite.js
@@ -0,0 +1,8 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+function opposite(number) {
+    // let OppositeNumb =number;
+    // return OppositeNumb.map((ele)=> -ele );
+    return -number;
+    
+}
+console.log(opposite(-1));
